4f92500101487b698002430e71332251c86160dc
allanderek <allan.clark@gmail.com>
2018-11-16 15:58:11 +0000

    Draw a more sensible pitch based on the tracab meta data.

--- src/VisualiseOpta.hs
+++ src/VisualiseOpta.hs
@@ -21,7 +21,9 @@ animate :: Tracab.Metadata -> F24.Game Tracab.Coordinates -> IO ()
 animate meta game =
     Gloss.simulate display color fps initialModel draw advance
     where
-    draw frame = Gloss.pictures [ pitchDrawing, drawFrame game frame ]
+    draw frame =
+        Gloss.Scale 5 5
+        $ Gloss.pictures [ pitchDrawing, drawFrame game frame ]
     advance = advanceModel game
     initialModel = F24.events game
     fps = 1
@@ -30,10 +32,13 @@ animate meta game =
     pitchDrawing =
         Gloss.Color Colors.pitch
         $ Gloss.Polygon [ topLeft, topRight, bottomRight, bottomLeft]
-    topLeft = (-100, -100)
-    topRight = (100, -100)
-    bottomRight = (100, 100)
-    bottomLeft = (-100, 100)
+
+    halfPitchLength = (Tracab.pitchSizeX meta) / 2.0
+    halfPitchWidth = (Tracab.pitchSizeY meta) / 2.0
+    topLeft = (0 - halfPitchLength, halfPitchWidth)
+    topRight = (halfPitchLength, halfPitchWidth)
+    bottomRight = (halfPitchLength, 0 - halfPitchWidth)
+    bottomLeft = (0 - halfPitchLength, 0 - halfPitchWidth)
 
 
 type Model = [ F24.Event Tracab.Coordinates ]
@@ -51,9 +56,9 @@ drawEvent game event =
     Gloss.Pictures pictures
     where
     pictures = [ eventId, typeId, periodId, eventPlace ]
-    eventId = Gloss.Translate (-200) 250 $ intLabel "eventId" $ F24.event_id event
-    typeId = Gloss.Translate    200  250 $ intLabel "typeId" $ F24.type_id event
-    periodId = Gloss.Translate    0  250 $ intLabel "periodId" $ F24.period_id event
+    eventId = Gloss.Translate (-40) 50 $ intLabel "eventId" $ F24.event_id event
+    typeId = Gloss.Translate    40  50 $ intLabel "typeId" $ F24.type_id event
+    periodId = Gloss.Translate   0  50 $ intLabel "periodId" $ F24.period_id event
     eventPlace =
         case F24.coordinates event of
             Nothing ->
@@ -62,13 +67,13 @@ drawEvent game event =
                 Gloss.Color (Gloss.makeColor 0.1 0.1 0.9 1.0)
                 $ Visualise.translateCoordinates coordinates
                 $ Gloss.ThickCircle radius thickness
-    radius = 5
-    thickness = 3
+    radius = 0.5
+    thickness = 0.3
 
     intLabel :: String -> Int -> Gloss.Picture
     intLabel title value =
         Gloss.Color Colors.writing
-        $ Gloss.Scale 0.2 0.2
+        $ Gloss.Scale 0.05 0.05
         $ Gloss.Text (title ++ ": " ++ (show value))
 
 
