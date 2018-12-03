af521c87031b6f06632da5a3cdc017c24728aa4a
allanderek <allan.clark@gmail.com>
2018-12-03 10:30:07 +0000

    Reinstate the actual work done in main of SyncMain.hs

--- src/SyncMain.hs
+++ src/SyncMain.hs
@@ -23,14 +23,14 @@ main = do
     tbData <- Tracab.parseDataFile tbMeta "data/tracab/803174_Man City-Chelsea.dat"
     f24Raw <- F24.loadGameFromFile "data/f24/f24-8-2015-803174-eventdetails.xml"
     let flippedFirstHalf = Tracab.rightToLeftFirstHalf tbData
+    let f24Data = F24.convertCoordinates flippedFirstHalf tbMeta f24Raw
+    let events = filter (\e -> F24.period_id e == 1) (F24.events f24Data)
+    let p1start = (Tracab.startFrame . head . Tracab.periods) tbMeta
+    let p1end = (Tracab.endFrame . head . Tracab.periods) tbMeta
+    let frames = filter (\f -> (Tracab.frameId f <= p1end) && (Tracab.frameId f >= p1start)) tbData
+    let gap = 1.0
+    let sync = Nw.align events frames sim gap
     putStrLn $ show flippedFirstHalf
-    -- let f24Data = F24.convertCoordinates flippedFirstHalf tbMeta f24Raw
-    -- let events = filter (\e -> F24.period_id e == 1) (F24.events f24Data)
-    -- let p1start = (Tracab.startFrame . head . Tracab.periods) tbMeta
-    -- let p1end = (Tracab.endFrame . head . Tracab.periods) tbMeta
-    -- let frames = filter (\f -> (Tracab.frameId f <= p1end) && (Tracab.frameId f >= p1start)) tbData
-    -- let gap = 1.0
-    -- let sync = Nw.align events frames sim gap
-    -- putStrLn $ show $ length frames
-    -- putStrLn $ show $ length events
-    -- putStrLn $ show $ length sync
+    putStrLn $ show $ length frames
+    putStrLn $ show $ length events
+    putStrLn $ show $ length sync
