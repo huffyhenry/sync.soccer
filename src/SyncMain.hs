60aaf78cc4f3d2e4b4c2b943563fdc24a94555ef
Marek Kwiatkowski <marek@mareklab.org>
2018-12-03 16:09:20 +0100

    Correct calls to statistical functions

--- src/SyncMain.hs
+++ src/SyncMain.hs
@@ -3,6 +3,7 @@
 import qualified Data.IntMap as Map
 import Data.Maybe (fromJust)
 import System.Environment (getArgs)
+import Statistics.Distribution (logDensity)
 import Statistics.Distribution.Normal as Gaussian
 import qualified Tracab
 import qualified F24
@@ -13,7 +14,7 @@ clockScore :: Double -> Double -> F24.Event Tracab.Coordinates -> Tracab.Frame -
 clockScore scale offset e f =
     let seconds = fromIntegral $ 60 * (F24.min e) + (F24.sec e)
         dist = abs $ seconds - (fromJust $ Tracab.clock f)
-    in Gaussian.standard.logDensity((dist - offset) / scale)
+    in logDensity Gaussian.standard ((dist - offset) / scale)
 
 locationScore :: Double -> F24.Event Tracab.Coordinates -> Tracab.Frame -> Double
 locationScore scale e f =
@@ -24,10 +25,10 @@ locationScore scale e f =
         xDist = fromIntegral $ eX - fX
         yDist = fromIntegral $ eY - fY
         dist = sqrt $ xDist**2.0 + yDist**2.0
-    in Gaussian.standard.logDensity(dist / scale)
+    in logDensity Gaussian.standard (dist / scale)
 
 totalScore :: Double -> F24.Event Tracab.Coordinates -> Tracab.Frame -> Double
-totalScore offset e f = clockScore offset e f + locationScore e f
+totalScore offset e f = (clockScore 1.0 offset e f) + (locationScore 1.0 e f)
 
 
 main :: IO ()
@@ -41,6 +42,6 @@ main = do
     let p1start = (Tracab.startFrame . head . Tracab.periods) tbMeta
     let p1end = (Tracab.endFrame . head . Tracab.periods) tbMeta
     let frames = filter (\f -> (Tracab.frameId f <= p1end) && (Tracab.frameId f >= p1start)) tbData
-    let gap = -1.0
-    let sync = NW.align events frames (penalty 0.0) gap
+    let gap = -10.0
+    let sync = NW.align events frames (totalScore 0.0) gap
     putStrLn $ show sync
