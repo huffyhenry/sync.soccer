ed02c4c5b6e42dab4fbcf1df010030d5be06cc69
allanderek <allan.clark@gmail.com>
2018-12-27 11:41:19 +0000

    Use do-notation for a the 'Nothing's in the calculation of euclidean distance.

--- src/Scoring.hs
+++ src/Scoring.hs
@@ -6,25 +6,18 @@ import qualified F24
 
 eventPlayerDistance :: F24.Event Tracab.Coordinates -> Tracab.Frame Tracab.Positions -> Maybe Double
 eventPlayerDistance event frame =
-    case F24.player_id event of
-        Nothing ->
-            Nothing
-        Just playerId ->
-            let
-                playerPosition = Data.List.find isPlayer (Tracab.agents $ Tracab.positions frame)
-                isPlayer p =
-                    Tracab.participantId p == playerId
-            in
-            case playerPosition of
-                Nothing ->
-                    Nothing
-                Just position ->
-                    Just distance
-                    where
-                    distance = sqrt (fromIntegral $ (squareSide playerX ballX) + (squareSide playerY ballY))
-                    squareSide p b = (p - b) ^ 2
-                    playerX = Tracab.x $ Tracab.coordinates position
-                    playerY = Tracab.y $ Tracab.coordinates position
-                    ballCoords = Tracab.coordinates $ Tracab.ball $ Tracab.positions frame
-                    ballX = Tracab.x ballCoords
-                    ballY = Tracab.y ballCoords
+    do
+        playerId <- F24.player_id event
+        let isPlayer p = Tracab.participantId p == playerId
+        playerPosition <- Data.List.find isPlayer (Tracab.agents $ Tracab.positions frame)
+        return $ euclideanDistance playerPosition
+    where
+    euclideanDistance playerPosition =
+        sqrt (fromIntegral $ (squareSide playerX ballX) + (squareSide playerY ballY))
+        where
+        squareSide p b = (p - b) ^ 2
+        ballX = Tracab.x ballCoords
+        ballY = Tracab.y ballCoords
+        playerX = Tracab.x $ Tracab.coordinates playerPosition
+        playerY = Tracab.y $ Tracab.coordinates playerPosition
+        ballCoords = Tracab.coordinates $ Tracab.ball $ Tracab.positions frame
