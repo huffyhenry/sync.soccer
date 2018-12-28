dc28d57c25ebc9bb794493d628184045a6b22f9d
allanderek <allan.clark@gmail.com>
2018-12-28 17:31:04 +0000

    A little refactoring of main to allow for parallel execution.

--- src/SyncMain.hs
+++ src/SyncMain.hs
@@ -33,6 +33,8 @@ locationScore scale e f =
 totalScore :: Double -> F24.Event Tracab.Coordinates -> Tracab.Frame -> Double
 totalScore offset e f = (clockScore 1.0 offset e f) + (locationScore 100.0 e f)
 
+type ScoringFunction = F24.Event Tracab.Coordinates -> Tracab.Frame -> Double
+
 -- Command line parsing machinery
 data Options = Options {
     tcbMetaFile :: String,
@@ -63,28 +65,60 @@ main = do
     f24Raw <- F24.loadGameFromFile (f24File opts)
     let flippedFirstHalf = Tracab.rightToLeftFirstHalf tbData
     let f24Data = F24.convertGameCoordinates flippedFirstHalf tbMeta f24Raw
-    let events = filter (\e -> F24.period_id e == 1) (F24.events f24Data)
-    let p1start = (Tracab.startFrame . head . Tracab.periods) tbMeta
-    let p1end = (Tracab.endFrame . head . Tracab.periods) tbMeta
-    let frames = filter (\f -> (Tracab.frameId f <= p1end) && (Tracab.frameId f >= p1start)) tbData
 
-    -- Take just the first ~20 minutes to stay under 16GB RAM for now.
-    let events2 = filter (\e -> F24.min e < 20) events
-    let frames2 = take (25*60*25) frames
+    let allEvents = F24.events f24Data
+    let allFrames = tbData
+    let (firstHalfEvents, firstHalfFrames) = getEventsAndFrames 1 (Just 5) tbMeta allEvents allFrames
+    -- This stackoverflow question explains why we need do `a par b pseq a+b`.
+    -- https://stackoverflow.com/questions/4576734/why-do-we-need-seq-or-pseq-with-par-in-haskell
 
-    -- The penalty for leaving frames unaligned needs to be small.
-    -- Conversely, leaving events unaligned should be costly.
-    -- Note that the score for a Match is negative on the log-density scale.
-    let gapl = \f -> (-10.0)    -- Leaves a frame unaligned for p < exp(-10) = 4.5e-5
-    let gapr = \e -> (-1000.0)
-    let sim = if timeOnly opts then clockScore 1.0 0.0 else totalScore 0.0
-    let sync = NW.align events2 frames2 sim gapl gapr
+
+    let scoring = if timeOnly opts then clockScore 1.0 0.0 else totalScore 0.0
+    let sync = alignEventsAndFrames scoring firstHalfEvents firstHalfFrames
     putStrLn $ show sync
-    putStr $ (show $ length frames2) ++ " frames, "
-    putStr $ (show $ length events2) ++ " events, "
-    putStrLn $ "total score " ++ show (NW.alignmentScore sim gapl gapr sync) ++ "."
+    putStr $ (show $ length firstHalfEvents) ++ " frames, "
+    putStr $ (show $ length firstHalfFrames) ++ " events, "
+    putStrLn $ "total score " ++ show (NW.alignmentScore scoring gapl gapr sync) ++ "."
 
     -- Write parsed data to CSVs for animation
-    CSV.frames2Csv frames2 "data/csv/frames.csv"
-    CSV.events2Csv events2 "data/csv/events.csv"
+    CSV.frames2Csv firstHalfFrames "data/csv/frames.csv"
+    CSV.events2Csv firstHalfEvents "data/csv/events.csv"
     CSV.alignment2Csv sync (outputFile opts)
+
+
+getEventsAndFrames :: Int -> Maybe Int -> Tracab.Metadata -> [F24.Event Tracab.Coordinates] -> [Tracab.Frame] -> ([F24.Event Tracab.Coordinates], [Tracab.Frame])
+getEventsAndFrames period mMinutes tbMeta allEvents allFrames =
+    case mMinutes of
+        Nothing ->
+            (events, frames)
+        Just minutes ->
+            ( filter (\e -> F24.min e < minLimit) events
+            , take (25*60*minLimit) frames
+            )
+            where
+            -- TODO: This will obviously not work for extra-time periods, we're basically assuming
+            -- that we'll only ever wish to restrict the minutes in the case that we're testing and
+            -- hence we just won't bother with extra time.
+            minLimit = minutes + ((period - 1) * 45)
+    where
+    events = filter (\e -> F24.period_id e == period) allEvents
+    frames = filter isInPeriod allFrames
+    isInPeriod f =
+        (Tracab.frameId f <= p1end) && (Tracab.frameId f >= p1start)
+    p1start = Tracab.startFrame tracabPeriod
+    p1end = Tracab.endFrame tracabPeriod
+    tracabPeriod = periods !! (period -1)
+    periods = Tracab.periods tbMeta
+
+-- The penalty for leaving frames unaligned needs to be small.
+-- Conversely, leaving events unaligned should be costly.
+-- Note that the score for a Match is negative on the log-density scale.
+gapl :: Tracab.Frame -> Double
+gapl = \f -> (-10.0)    -- Leaves a frame unaligned for p < exp(-10) = 4.5e-5
+gapr :: F24.Event Tracab.Coordinates -> Double
+gapr = \e -> (-1000.0)
+
+alignEventsAndFrames :: ScoringFunction -> [F24.Event Tracab.Coordinates] -> [Tracab.Frame] -> NW.Alignment (F24.Event Tracab.Coordinates) Tracab.Frame
+alignEventsAndFrames scoring events frames =
+    NW.align events frames scoring gapl gapr
+    where
